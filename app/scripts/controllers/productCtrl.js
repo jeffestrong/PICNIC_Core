fab40267e1f6b131865c7d20b1b60f053f6067e1
Sankhadeep Roy <sankhadeeproy@Sankhadeeps-MacBook-Air.local>
2015-08-19 19:45:11 +0530

    changes to navbar and product

--- app/scripts/controllers/productCtrl.js
+++ app/scripts/controllers/productCtrl.js
@@ -10,11 +10,11 @@
  
  angular.module('AniTheme').controller('productControl', function ($scope) {
  	$scope.products = [
- 	{url:'images/food1.jpg', desc: 'Lorem ipsum dolor sit amet, consectetur quidem?Lorem ipsum dolor sit amet, consectetur quidem?Lorem ipsum dolor sit amet, consectetur quidem?Lorem ipsum dolor sit amet, consectetur quidem?'}, 
- 	{url:'images/food2.jpg', desc: 'Lorem ipsum dolor sit amet, consectetur Voluptate!'},         
- 	{url:'images/food3.jpg', desc: 'Lorem ipsum dolor sit amet, consectetur nulla'},         
- 	{url: 'images/food4.jpg', desc: 'Lorem ipsum dolor sit amet, consectetur odit'},
- 	{url: 'images/food5.jpg', desc: 'Lorem ipsum dolor sit amet, consectetur Aliquid!'},
- 	{url: 'images/food6.jpg', desc: 'Lorem ipsum dolor sit amet, consectetur a!'}
+ 	{url:'images/food1.jpg', head:'Header is here', desc: 'Lorem ipsum dolor sit amet, consectetur quidem?Lorem ipsum dolor sit amet, consectetur quidem?Lorem ipsum dolor sit amet, consectetur quidem?Lorem ipsum dolor sit amet, consectetur quidem?'}, 
+ 	{url:'images/food2.jpg', head:'Header is here', desc: 'Lorem ipsum dolor sit amet, consectetur Voluptate!'},         
+ 	{url:'images/food3.jpg', head:'Header is here', desc: 'Lorem ipsum dolor sit amet, consectetur nulla'},         
+ 	{url: 'images/food4.jpg', head:'Header is here', desc: 'Lorem ipsum dolor sit amet, consectetur odit'},
+ 	{url: 'images/food5.jpg', head:'Header is here', desc: 'Lorem ipsum dolor sit amet, consectetur Aliquid!'},
+ 	{url: 'images/food6.jpg', head:'Header is here', desc: 'Lorem ipsum dolor sit amet, consectetur a!'}
  	];
  });
